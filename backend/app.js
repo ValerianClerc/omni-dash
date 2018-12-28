ca896a255a7e3c8499cb9919eb987dcadec69cc9
Valerian Clerc <valerian.clerc@mail.mcgill.ca>
2018-12-28 11:55:51 -0500

    frontend cli init

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,11 @@
 const express = require('express');
 
-const app = express()
+const app = express();
 
 app.get('/', (req, res) => {
-    res.send('HOME ROUTE')
-})
+    res.send('HOME ROUTE');
+});
 
 app.listen(3000, () => {
-    console.log('Server started on port 3000')
-})
+    console.log('Server started on port 3000');
+});
